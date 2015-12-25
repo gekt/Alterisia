2de7d9c274f7205827162edcecdffd9c8b995b37
gekt <ktgeneve97@hotmail.com>
2015-12-25 19:22:47 +0100

    clean du code

--- index.php
+++ index.php
@@ -1,48 +1,48 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Altérisia</title>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <head>
+        <title>Altérisia</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
         <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
         <script src="#"></script> <!-- [renseigner le futur js] -->
-	</head>
-		<body>
-			<header class"header"> <!-- [Header global logo, nom site et login API steam] -->
-				<section class="header_contenu"><!-- [Contenu du header avec les different bloc] -->
-					<div class="logo"><!-- [logo png] -->
-						
-					</div>
-					<div class="nom"><!-- [nom site texte ou image png] -->
-						
-					</div>
-					<div class="login"><!-- [login steam API pour les sessions] -->
-						
-					</div>			
-				</section>
-			</header>
-				<section class="contenu_page"><!-- [Contenu de la page avec les differents blocs serveur] -->
-					<div class="b-1"><!-- [Bloc serveur 1] -->
-						
-					</div>
-					<div class="b-2"><!-- [Bloc serveur 2] -->
-						
-					</div>
-					<div class="b-3"><!-- [Bloc serveur 3] -->
-						
-					</div>
-					<div class="bt-forum"><!-- [bouton forum] -->
-						
-					</div>		
-				</section>
-			<footer class"footer"><!-- [Footer global] -->
-					<div class="footer_info"><!-- [Footer avec info defilante] -->
-						
-					</div>
-					<div class="copyright"><!-- [Footer copyright] -->
-						
-					</div>
-			</footer>
-		</body>
+    </head>
+        <body>
+            <header class"header"> <!-- [Header global logo, nom site et login API steam] -->
+                <section class="header_contenu"><!-- [Contenu du header avec les different bloc] -->
+                    <div class="logo"><!-- [logo png] -->
+                        
+                    </div>
+                    <div class="nom"><!-- [nom site texte ou image png] -->
+                        
+                    </div>
+                    <div class="login"><!-- [login steam API pour les sessions] -->
+                        
+                    </div>
+                </section>
+            </header>
+                <section class="contenu_page"><!-- [Contenu de la page avec les differents blocs serveur] -->
+                    <div class="b-1"><!-- [Bloc serveur 1] -->
+                        
+                    </div>
+                    <div class="b-2"><!-- [Bloc serveur 2] -->
+                        
+                    </div>
+                    <div class="b-3"><!-- [Bloc serveur 3] -->
+                        
+                    </div>
+                    <div class="bt-forum"><!-- [bouton forum] -->
+                        
+                    </div>
+                </section>
+            <footer class"footer"><!-- [Footer global] -->
+                    <div class="footer_info"><!-- [Footer avec info defilante] -->
+                        
+                    </div>
+                    <div class="copyright"><!-- [Footer copyright] -->
+                        
+                    </div>
+            </footer>
+        </body>
 </html>
